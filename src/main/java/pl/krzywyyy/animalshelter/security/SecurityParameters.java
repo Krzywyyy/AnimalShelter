ef24e1e3fd11608469975150eaf0da14f949eb9a
Krzywyyy <piotr.kostrzewski97@gmail.com>
2022-02-06 12:52:49 +0100

    added management role to enum and changed login url

--- src/main/java/pl/krzywyyy/animalshelter/security/SecurityParameters.java
+++ src/main/java/pl/krzywyyy/animalshelter/security/SecurityParameters.java
@@ -11,8 +11,9 @@ public class SecurityParameters {
     public final String UTF_CHARACTER_ENCODING = "UTF-8";
     public final String CLIENT_ROLE = "CLIENT";
     public final String EMPLOYEE_ROLE = "EMPLOYEE";
+    public final String MANAGEMENT_ROLE = "MANAGEMENT";
     public final String ADMIN_ROLE = "ADMIN";
     public final String USER_NOT_AUTHORIZED_EXCEPTION_MESSAGE = "Authentication failed";
     public final long TOKEN_EXPIRATION_TIME = 364_000_000;
-    public final String LOGIN_URL = "/users/login";
+    public final String LOGIN_URL = "/login";
 }
